c748430e0eeeb9a0965ec351b6085c1d8bbcec24
Rory Dowse <rorydowse@hotmail.com>
2025-01-18 18:50:23 -0700

    add Sticky CTA styling

--- src/app/components/StickyCTA.tsx
+++ src/app/components/StickyCTA.tsx
@@ -2,18 +2,25 @@ import { InfinityRewardsCard } from "@/app/models/CreditCard";
 
 export default function StickyCTA() {
     return (
-        <div>
-            <p>{InfinityRewardsCard.name}</p>
-            <a href={InfinityRewardsCard.callToAction?.[0].termsLink}>Terms and Conditions</a>
+        <div className="sticky bottom-0 bg-blue-900 text-white py-5 pl-3">
+            <p className="font-bold">{InfinityRewardsCard.name}</p>
+            <a 
+                href={InfinityRewardsCard.callToAction?.[0].termsLink}
+                className="underline text-sm">
+                    Terms and Conditions
+                </a>
             <br />
-            <button>
-                <a 
-                    href={InfinityRewardsCard.callToAction?.[0].applyNowLink} 
-                    aria-label={`Apply Now for the ${InfinityRewardsCard.name}`}
-                    >
-                        Apply Now
-                    </a>
-            </button>
+            <div className="absolute right-3 bottom-8">
+                <button>
+                    <a 
+                        href={InfinityRewardsCard.callToAction?.[0].applyNowLink} 
+                        aria-label={`Apply Now for the ${InfinityRewardsCard.name}`}
+                        className="bg-blue-500 text-white font-bold py-4 px-4 rounded hover:bg-blue-700 transition duration-300 ease-in-out"
+                        >
+                            Apply Now
+                        </a>
+                </button>
+            </div>
         </div>
     )
 }
