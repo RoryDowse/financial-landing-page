388a4f51192a46ca2ec5f4eeba3eae94c6a279ae
Rory Dowse <rorydowse@hotmail.com>
2025-01-20 18:24:35 -0700

    add Swiper

--- src/app/components/HowItWorks.tsx
+++ src/app/components/HowItWorks.tsx
@@ -1,4 +1,14 @@
+'use client';
+
+import { Pagination, Navigation, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import Image from "next/image";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 import { InfinityRewardsCard } from "@/app/models/CreditCard";
 
@@ -7,9 +17,21 @@ export default function HowItWorks() {
         <section>
             <h2>How To Redeem Your Rewards</h2>
 
-            {/* Add slider */}
+            {/* Swiper */}
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
 
             {/* Slide One */}
+            <SwiperSlide>
             <Image
                 src={`${InfinityRewardsCard.howItWorks?.[0].image}`}
                 alt={`${InfinityRewardsCard.howItWorks?.[0].redeemMethod} Image`}
@@ -18,8 +40,10 @@ export default function HowItWorks() {
                 />
                 <p>{InfinityRewardsCard.howItWorks?.[0].redeemMethod}</p>
                 <p>{InfinityRewardsCard.howItWorks?.[0].description}</p>
+            </SwiperSlide>
 
             {/* Slide Two */}
+            <SwiperSlide>
             <Image
                 src={`${InfinityRewardsCard.howItWorks?.[1].image}`}
                 alt={`${InfinityRewardsCard.howItWorks?.[1].redeemMethod} Image`}
@@ -28,8 +52,10 @@ export default function HowItWorks() {
                 />
                 <p>{InfinityRewardsCard.howItWorks?.[1].redeemMethod}</p>
                 <p>{InfinityRewardsCard.howItWorks?.[1].description}</p>
+            </SwiperSlide>
 
             {/* Slide Three */}
+            <SwiperSlide>
             <Image
                 src={`${InfinityRewardsCard.howItWorks?.[2].image}`}
                 alt={`${InfinityRewardsCard.howItWorks?.[2].redeemMethod} Image`}
@@ -38,6 +64,8 @@ export default function HowItWorks() {
                 />
                 <p>{InfinityRewardsCard.howItWorks?.[2].redeemMethod}</p>
                 <p>{InfinityRewardsCard.howItWorks?.[2].description}</p>
+            </SwiperSlide>
+            </Swiper>
         </section>
     );
 }
