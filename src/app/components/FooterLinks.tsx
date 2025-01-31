2ad8c521bc40675c5536cb86e3f6fa6ed06c739c
Rory Dowse <rorydowse@hotmail.com>
2025-01-31 15:06:04 -0700

    update layout.tsx

--- src/app/components/FooterLinks.tsx
+++ src/app/components/FooterLinks.tsx
@@ -1,12 +1,12 @@
 
 const footerLinks = [
-    { text: "About Us", href: "" },
-    { text: "Contact Us", href: "" },
-    { text: "Privacy Policy", href: "" },
-    { text: "Terms and Conditions", href: "" },
-    { text: "Credit Cards" , href: "" },
-    { text: "Rewards Program", href: "" },
-    { text: "Sitemap", href: "" },
+    { text: "About Us", href: "/" },
+    { text: "Contact Us", href: "/" },
+    { text: "Privacy Policy", href: "/" },
+    { text: "Terms and Conditions", href: "/" },
+    { text: "Credit Cards" , href: "/" },
+    { text: "Rewards Program", href: "/" },
+    { text: "Sitemap", href: "/" },
 ];
 
 export default footerLinks;
