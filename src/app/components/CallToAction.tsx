574722c02bb6be51acdd83dc84cccf84e8151b23
Rory Dowse <rorydowse@hotmail.com>
2025-02-01 08:44:00 -0700

    update login button size on lg

--- src/app/components/CallToAction.tsx
+++ src/app/components/CallToAction.tsx
@@ -20,7 +20,7 @@ export default function CallToAction() {
             </div>
             <p className="text-xl text-yellow-400 mb-6">{callToAction?.description}</p>
             <button 
-                className="bg-yellow-400 text-blue-900 text-lg font-bold mt-4 py-4 px-6 rounded-lg shadow-md hover:bg-yellow-300 transition-colors duration-300"
+                className="bg-yellow-400 text-blue-900 text-lg font-bold mt-4 py-4 px-8 rounded-lg shadow-md hover:bg-yellow-300 transition-colors duration-300"
                 aria-label={`Apply Now for the ${InfinityRewardsCard.name} Credit Card`}
                 >
                 <a
