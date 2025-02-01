c6207cc4500f6d186e89cbf69267b80476165781
Rory Dowse <rorydowse@hotmail.com>
2025-02-01 10:31:55 -0700

    add OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,36 @@
-// import Head from 'next/head';
 import '@styles/globals.css';
 
 export const metadata = {
+  title: "Infinity Rewards Credit Card",
+  description: "Get rewards for every dollar you spend.",
+  openGraph: {
+    title: "Infinity Rewards Credit Card | Empower Bank",
+    description: "Earn rewards with every purchase. Sign up now!",
+    url: "https://financial-landing-page.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://financial-landing-page.vercel.app/images/infinity-rewards-credit-card.webp",
+        width: 1200,
+        height: 630,
+        alt: "Infinity Rewards Credit Card",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@RoryDowseCoding",
     title: "Infinity Rewards Credit Card",
-    description: "Get rewards for every dollar you spend.",
-  };
-  
-  export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="en">
-        <head />
-        <body>{children}</body>
-      </html>
-    );
+    description: "Earn rewards with every purchase. Sign up now!",
+    image: "https://financial-landing-page.vercel.app/images/infinity-rewards-credit-card.webp",
   }
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
