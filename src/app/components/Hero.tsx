c4ed4421320275184122b01c353f1516d2ac2198
Rory Dowse <rorydowse@hotmail.com>
2025-02-02 16:42:02 -0700

    add small screen size

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -19,12 +19,12 @@ export default function Hero() {
                     height={309}
                     priority // Preloads image for better performance (no need for eager loading)
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    className="mx-auto object-cover w-full h-full md:w-1/2 md:h-1/2 lg:w-3/4 lg:h-3/4 border-t-yellow-400 border-t-2 border-r-yellow-400 border-r-2 rounded-2xl shadow-xl"
+                    className="mx-auto object-cover sm:w-full sm:h-full md:w-1/2 md:h-1/2 lg:w-3/4 lg:h-3/4 border-t-yellow-400 border-t-2 border-r-yellow-400 border-r-2 rounded-2xl shadow-xl"
                     quality={90} // Serve high-quality images
                 />
                 )}
                 {/* Gradient overlay */}
-                <div className="absolute inset-0 bg-gradient-to-tr from-white to-transparent to-60% opacity-50 rounded-2xl pointer-events-none mx-auto w-full h-full md:w-1/2 md:h-full lg:w-3/4 lg:h-full"></div>
+                <div className="absolute inset-0 bg-gradient-to-tr from-white to-transparent to-60% opacity-50 rounded-2xl pointer-events-none mx-auto sm:w-full sm:h-full md:w-1/2 md:h-full lg:w-3/4 lg:h-full"></div>
             </div>
 
             {/* Hero Content */}
