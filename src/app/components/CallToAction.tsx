9597883ab0c2e7ce0884ecf25b874fcd39b8175b
Rory Dowse <rorydowse@hotmail.com>
2025-02-02 16:47:12 -0700

    update CallToAction aria labels

--- src/app/components/CallToAction.tsx
+++ src/app/components/CallToAction.tsx
@@ -21,11 +21,11 @@ export default function CallToAction() {
             <p className="text-xl text-yellow-400 mb-6">{callToAction?.description}</p>
             <button 
                 className="bg-yellow-400 text-blue-900 text-lg font-bold mt-4 py-4 px-8 rounded-lg shadow-md hover:bg-yellow-300 transition-colors duration-300"
-                aria-label={`Apply Now for the ${InfinityRewardsCard.name} Credit Card`}
+                aria-label={`Apply Now for the ${InfinityRewardsCard.name}`}
                 >
                 <a
                     href={callToAction?.applyNowLink}
-                    aria-label={`Apply Now for the ${InfinityRewardsCard.name} Credit Card`}
+                    aria-label={`Apply Now for the ${InfinityRewardsCard.name}`}
                     className="no-underline"
                 >
                     Apply Now
